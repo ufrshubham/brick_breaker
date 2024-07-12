4b739b7d9cd22276f4527470a8fe8e43ab65987c
DevKage <ufrshubham@gmail.com>
2024-07-12 22:53:40 +0530

    Add walls

--- lib/game/actors/walls.dart
+++ lib/game/actors/walls.dart
@@ -1,1 +1,50 @@
+import 'package:flame_forge2d/flame_forge2d.dart';
 
+class Walls extends BodyComponent {
+  Walls(Vector2 gameSize, double wallThickness)
+      : super(
+          bodyDef: BodyDef(type: BodyType.static),
+          fixtureDefs: [
+            FixtureDef(
+              PolygonShape()
+                ..setAsBox(
+                  gameSize.x * 0.5,
+                  wallThickness * 0.5,
+                  Vector2(gameSize.x * 0.5, wallThickness * 0.5),
+                  0,
+                ),
+              restitution: 1,
+            ),
+            FixtureDef(
+              PolygonShape()
+                ..setAsBox(
+                  wallThickness * 0.5,
+                  gameSize.y * 0.5,
+                  Vector2(wallThickness * 0.5, gameSize.y * 0.5),
+                  0,
+                ),
+              restitution: 1,
+            ),
+            FixtureDef(
+              PolygonShape()
+                ..setAsBox(
+                  wallThickness * 0.5,
+                  gameSize.y * 0.5,
+                  Vector2(gameSize.x - wallThickness * 0.5, gameSize.y * 0.5),
+                  0,
+                ),
+              restitution: 1,
+            ),
+            FixtureDef(
+              PolygonShape()
+                ..setAsBox(
+                  gameSize.x * 0.5,
+                  wallThickness * 0.5,
+                  Vector2(gameSize.x * 0.5, gameSize.y - wallThickness * 0.5),
+                  0,
+                ),
+              isSensor: true,
+            ),
+          ],
+        );
+}
--- lib/game/world.dart
+++ lib/game/world.dart
@@ -1,22 +1,20 @@
 import 'dart:async';
 
+import 'package:brick_breaker/game/actors/walls.dart';
 import 'package:brick_breaker/game/game.dart';
+import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
-class BrickBreakerWorld extends Forge2DWorld {
+class BrickBreakerWorld extends Forge2DWorld
+    with HasGameReference<BrickBreakerGame> {
   BrickBreakerWorld() : super(gravity: Vector2.zero());
 
   @override
   Future<void> onLoad() async {
     await add(
-      BodyComponent(
-        bodyDef: BodyDef(type: BodyType.dynamic),
-        fixtureDefs: [
-          FixtureDef(
-            CircleShape()..radius = 20 * BrickBreakerGame.lengthFactor,
-            restitution: 1,
-          )
-        ],
+      Walls(
+        game.camera.viewport.virtualSize,
+        5 * BrickBreakerGame.lengthFactor,
       ),
     );
   }
